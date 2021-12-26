3cef6914c96e614068238a7ed080a50f342e04e5
Elias Rafael <78946129+ER-idk@users.noreply.github.com>
2021-12-26 01:05:33 -0300

    Update CaesarShift.py

--- Caesar Shift/CaesarShift.py
+++ Caesar Shift/CaesarShift.py
@@ -4,14 +4,17 @@ def caesar_encode(text, n):
     encode = str()
     w = len(text)
     for i in range(w):
+        # Encontra o indice no alfabeto
         i_alpha = alpha.index(text[i]) + n
+        
+        # Retorna ao inicio do alfabeto caso necessário
         if i_alpha >= len(alpha):
             i_alpha = i_alpha - len(alpha)
         encode += alpha[i_alpha]
 
     return encode
 
-
+# Para decodificar basta repetir o mesmo processo porém diminuindo o deslocamento
 def caesar_decode(text, n):
     decode = str()
     w = len(text)
